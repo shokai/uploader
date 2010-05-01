db1872eba7c1431bf88c74535eccf762a587a57e
Sho Hashimoto <hashimoto@shokai.org>
2010-05-02 03:46:14 +0900

    first commit

---
+++ server/main.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'rack'
+require 'json'
+
+post '/' do
+  p params
+  if params["data"] and params["file_ext"]
+    now = Time.now
+    ext = params['file_ext']
+    key = "#{now.to_i}_#{now.usec}.#{params['file_ext']}"
+    open(File.dirname(__FILE__) + "/public/#{key}", 'w+b'){|f|
+      f.write(params["data"])
+    }
+    @mes = {
+      'message' => 'success',
+      'key' => key
+    }.to_json
+  else
+    @mes = {"message" => 'error'}.to_json
+  end
+end
