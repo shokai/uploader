8e4a7ea71cfc98c6e0c80af24720f5c45fb78c52
Sho Hashimoto <hashimoto@shokai.org>
2010-05-02 11:09:56 +0900

    downcase ext

--- server/main.rb
+++ server/main.rb
@@ -10,9 +10,9 @@ require 'digest/md5'
 post '/' do
   if params["data"] and params["file_ext"]
     now = Time.now
-    ext = params['file_ext']
     time = "#{now.to_i}_#{now.usec}"
-    key = "#{Digest::MD5.hexdigest(time)}.#{params['file_ext']}"
+    ext = params['file_ext'].downcase
+    key = "#{Digest::MD5.hexdigest(time)}.#{ext}"
     open(File.dirname(__FILE__) + "/public/#{key}", 'w+b'){|f|
       f.write(params["data"])
     }
