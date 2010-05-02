cadb8f0b862c68d59b48e0e3e86913e20f770ff0
Sho Hashimoto <hashimoto@shokai.org>
2010-05-02 10:29:18 +0900

    use md5 hexdigest

--- server/main.rb
+++ server/main.rb
@@ -5,12 +5,13 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'rack'
 require 'json'
+require 'digest/md5'
 
 post '/' do
   if params["data"] and params["file_ext"]
     now = Time.now
     ext = params['file_ext']
-    key = "#{now.to_i}_#{now.usec}.#{params['file_ext']}"
+    key = "#{Digest::MD5.hexdigest(now.to_s)}.#{params['file_ext']}"
     open(File.dirname(__FILE__) + "/public/#{key}", 'w+b'){|f|
       f.write(params["data"])
     }
