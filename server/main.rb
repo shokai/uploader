3d0c93781c61f65d3e373172d6dc96bbbbf2a344
Sho Hashimoto <hashimoto@shokai.org>
2010-05-02 10:36:01 +0900

    fix md5

--- server/main.rb
+++ server/main.rb
@@ -11,7 +11,8 @@ post '/' do
   if params["data"] and params["file_ext"]
     now = Time.now
     ext = params['file_ext']
-    key = "#{Digest::MD5.hexdigest(now.to_s)}.#{params['file_ext']}"
+    time = "#{now.to_i}_#{now.usec}"
+    key = "#{Digest::MD5.hexdigest(time)}.#{params['file_ext']}"
     open(File.dirname(__FILE__) + "/public/#{key}", 'w+b'){|f|
       f.write(params["data"])
     }
