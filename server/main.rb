821c3c7228188f9f37487a0101cb0d80ff51a3da
Sho Hashimoto <hashimoto@shokai.org>
2010-05-04 01:48:07 +0900

    fix

--- server/main.rb
+++ server/main.rb
@@ -8,6 +8,7 @@ require 'json'
 require 'digest/md5'
 require 'yaml'
 
+
 begin
   @@config = YAML::load open(File.dirname(__FILE__) + '/config.yaml')
 rescue
@@ -25,23 +26,23 @@ def token?(token)
 end
 
 post '/' do
-  p params['token']
+  p params[:token]
   if !token?(params['token'])
     @mes = {'error' => 'token error'}.to_json
   else
-    if !params['data']
+    if !params[:data]
       @mes = {'error' => 'file data not found'}.to_json
     else
       now = Time.now
       time = "#{now.to_i}_#{now.usec}"
       key = "#{Digest::MD5.hexdigest(time)}"
-      key += "."+params['file_ext'].downcase if params['file_ext'] and params['file_ext'].size > 0
+      key += "."+params[:file_ext].downcase if params[:file_ext] and params[:file_ext].size > 0
       open(File.dirname(__FILE__) + "/public/#{key}", 'w+b'){|f|
-        f.write(params["data"])
+        f.write(params[:data])
         @mes = {
           'message' => 'success',
           'key' => key,
-          'size' => params['data'].size
+          'size' => params[:data].size
         }.to_json
       }
     end
